aede56ef4f4dc0ae1a3a6c0b41cf37a5bde38c2c
bralegz <bralegz@gmail.com>
2024-07-06 12:39:37 -0500

    keeping track of lives

--- hangman.py
+++ hangman.py
@@ -10,16 +10,29 @@ print(f"Pssst, the solution is {chosen_word}.")
 display = []
 for letter in chosen_word:
     display.append("_")
-    
+
+lives_left = 6
+
 while "_" in display:
-    # Prompt user to
-    guess = input("Guess a letter: ").lower()
+    blank_spaces_filled = 0
+    if lives_left > 0:
+        # Prompt user to
+        guess = input("Guess a letter: ").lower()
 
-    # replace blank spaces with letter
-    for index, letter in enumerate(chosen_word):
-        if letter == guess:
-            display[index] = guess
+        # replace blank spaces with letter
+        for index, letter in enumerate(chosen_word):
+            if letter == guess:
+                display[index] = guess
+                blank_spaces_filled += 1
+        if blank_spaces_filled > 0:
+            print(display)
+        else:
+            print("Wrong!")
+            lives_left -= 1
+    else:
+      break
 
-    print(display)
-    
-print("You won!")
+if lives_left > 0:      
+    print("You've won!")
+else:
+    print("Game over. You lose")
